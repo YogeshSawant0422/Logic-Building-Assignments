d8d2b7ddd93d36313abd3eec1698bc5efdc3844f
YogeshSawant0422 <yogeshsawant0422@gmail.com>
2021-04-24 23:07:31 +0530

    Assignment 01

---
+++ Assignment No. 1/02 Program To Display 5 Times Marvellous On Screen ..c
@@ -0,0 +1,18 @@
+// Program To Display 5 Times Marvellous On Screen .
+#include<stdio.h>
+
+void Display()
+{
+    int i = 0 ;
+
+    for( i = 1 ; i <=5 ; i++)
+    {
+        printf("\n Marvellous ");
+    }
+}
+void main()
+{
+    Display();
+
+    return 0;
+}
