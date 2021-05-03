a65f7252b8167a1268e64e525d35087b5a9f51ea
YogeshSawant0422 <yogeshsawant0422@gmail.com>
2021-05-03 23:21:35 +0530

    Assignment No 3

---
+++ Assignment No. 3/03 Write a program which accept number from user and print even factors of that number.c
@@ -0,0 +1,24 @@
+//Write a program which accept number from user and print even factors of that number.
+#include<stdio.h>
+{
+        Input : 36
+Output: 2 6 12 18
+void DisplayEvenFactor(int iNo)
+{
+int i = 0;
+ if(iNo <= 0)
+ {
+ iNo = -iNo;
+}
+
+ for(i = 1; i<= __ ;i++)
+{
+ if(____&&___)
+ {
+
+ }
+}
+}
+}
+
+
