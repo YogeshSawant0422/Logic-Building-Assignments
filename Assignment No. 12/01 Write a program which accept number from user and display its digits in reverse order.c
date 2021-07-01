bfbf94b88bbb7c03c50466e751626a1897901449
YogeshSawant0422 <yogeshsawant0422@gmail.com>
2021-07-01 22:33:07 +0530

    Assignment No 12

---
+++ Assignment No. 12/01 Write a program which accept number from user and display its digits in reverse order.c
@@ -0,0 +1,25 @@
+//Write a program which accept number from user and display its digits in reverse order.
+#include<stdio.h>
+void DisplayDigit(int iNo)
+{
+ int iDigit = 0;
+
+ if(________)
+ {
+ iNo = -iNo;
+ }
+ while(______)
+    {
+ iDigit = ____________;
+ printf(“%d”,iDigit);
+ ______ = _____ / ______;
+ }
+}
+int main()
+{
+ int iValue = 0;
+ printf(“Enter number”);
+ scanf(“%d”,&iValue);
+ DisplayDigit(iValue);
+ return 0;
+}
