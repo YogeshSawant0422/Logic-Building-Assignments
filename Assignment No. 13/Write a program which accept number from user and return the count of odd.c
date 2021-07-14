8ddd713d304dd40f633c4b355416ac498f8e552f
YogeshSawant0422 <yogeshsawant0422@gmail.com>
2021-07-14 22:46:42 +0530

    Assignment No 13

---
+++ Assignment No. 13/Write a program which accept number from user and return the count of odd.c
@@ -0,0 +1,37 @@
+// Write a program which accept number from user and return the count of odd digits
+#include<stdio.h>
+#include<conio.h>
+int CountOdd(int iNo)
+{
+      int iDig = 0 , iCnt = 0;
+
+        if(iNo < 0)
+        {
+            iNo = -iNo;
+        }
+       while(iNo > 0)
+        {
+            iDig = iNo % 10;
+            if(iNo > 0)
+            {
+                if(iDig % 2 != 0)
+                iCnt++;
+            }
+            iNo = iNo / 10;
+        }
+    return iCnt;
+}
+int main()
+{
+     int iValue = 0;
+     int iRet = 0;
+
+     printf("\n Enter number : ");
+     scanf("%d",&iValue);
+
+     iRet = CountOdd(iValue);
+     printf("\n Odd Count is : %d",iRet);
+
+      _getch();
+     return 0;
+}
