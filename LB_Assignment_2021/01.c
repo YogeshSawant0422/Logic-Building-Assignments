a7fd3ac74268823df58757c543c75cfbe039cb93
YogeshSawant0422 <yogeshsawant0422@gmail.com>
2021-09-22 23:39:07 +0530

    LB NEW

---
+++ LB_Assignment_2021/01.c
@@ -0,0 +1,22 @@
+
+#include<stdio.h>
+
+int main()
+{
+    int Arr[5];  // Array of 5 integers
+
+    int iSum = 0 , i = 0;
+
+    printf("Enter a number\n");
+
+    for(i = 1 ; i <= 5 ; i++)
+    {
+        scanf("%d",&Arr[i]);
+
+        iSum += Arr[i];
+    }
+
+    printf("Addition is : %d\n",iSum);
+
+    return 0;
+}
