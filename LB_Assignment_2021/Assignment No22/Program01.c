867e54d705b55eef3d2a82a2490ba0c2007359f0
YogeshSawant0422 <yogeshsawant0422@gmail.com>
2021-10-04 21:45:57 +0530

    LB Assignment

--- LB_Assignment_2021/Assignment No22/Program01.c
+++ LB_Assignment_2021/Assignment No22/Program01.c
@@ -1,1 +1,31 @@
 
+#include<stdio.h>
+int CountCapital(char *str)
+{
+    int iCnt = 0;
+
+    if(str == NULL)
+    {
+        return 0;
+    }
+
+    while(*str != '\0')
+    {
+            if(*str >= 'A' && *str <= 'Z')
+            {
+                iCnt++;
+            }
+            str++;
+    }
+    return iCnt ;
+}
+int main()
+{
+    char arr[20];
+    int iRet = 0;
+    printf("\n Enter string : ") ;
+    scanf("%[^'\n']s",arr);
+    iRet = CountCapital(arr);
+    printf("%d",iRet);
+    return 0;
+}
